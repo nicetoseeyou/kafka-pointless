b11098040feaa18d2bcb1a62f5df31cc2a9c1082
nicetoseeyou <hello4world@yeah.net>
2019-01-18 23:48:54 +0800

    initial

---
+++ src/main/java/lab/orz/pointless/kafka/App.java
@@ -0,0 +1,13 @@
+package lab.orz.pointless.kafka;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
